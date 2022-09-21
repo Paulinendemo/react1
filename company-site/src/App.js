9df747245a4b8dd569df73a26ae9cc0025923eb5
gracekyalo <gracendanu27@gmail.com>
2022-09-21 15:01:34 +0300

    services and portfolio

--- company-site/src/App.js
+++ company-site/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import './index.css'
 import Header from "./components/Header"
 import About from "./components/About";
+import Services from "./components/Services";
+
 import Pricing from "./components/Pricing";
 import Contact from "./components/Contact";
 import Map from "./components/Map";
 import Footer from "./components/Footer";
+import Portfolio from "./components/Portfolio";
 
 
 function App() {
@@ -14,6 +17,8 @@ function App() {
     <div>
       <Header/>
       <About/>
+      <Services/>
+      <Portfolio/>
       <Pricing />
       <Contact />
       <Map />
