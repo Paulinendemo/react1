9a41364d5024426f97bc8ad75bbc066e03bb5157
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-09-21 11:00:01 +0300

    mm

---
+++ company-site/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './App.css';
+import About from "./components/About";
+import Header from "./components/Header"
+function App() {
+  return (
+    <div>
+      <Header/>
+      <About/>
+    </div>
+  )
+}
+
+export default App;
